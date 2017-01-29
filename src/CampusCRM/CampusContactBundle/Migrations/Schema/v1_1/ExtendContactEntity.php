24a4f6a9b0abf24d275b27f9b82dc858d92166cc
Jake He <zhex900@gmail.com>
2017-01-29 23:34:22 +1100

    Add country of birth field.

---
+++ src/CampusCRM/CampusContactBundle/Migrations/Schema/v1_1/ExtendContactEntity.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jake
+ * Date: 27/1/17
+ * Time: 1:07 PM
+ */
+
+namespace CampusCRM\CampusContactBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class ExtendContactEntity implements Migration, ExtendExtensionAwareInterface
+{
+    protected $extendExtension;
+
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
+    }
+
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            'orocrm_contact',
+            'country_of_birth',
+            'oro_dictionary_country',
+            'name',
+            ['extend' => ['without_default' => true, 'is_extend' => true, 'owner' => ExtendScope::OWNER_CUSTOM]]
+        );
+    }
+}
