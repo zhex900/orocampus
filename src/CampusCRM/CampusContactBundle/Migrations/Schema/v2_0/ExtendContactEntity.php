c316a219bbe73852a519cae8a5763114db3af563
Jake He <zhex900@gmail.com>
2017-01-30 23:47:14 +1100

    remove tmp folder

--- src/CampusCRM/CampusContactBundle/Migrations/Schema/v2_0/ExtendContactEntity.php
+++ src/CampusCRM/CampusContactBundle/Migrations/Schema/v2_0/ExtendContactEntity.php
@@ -6,31 +6,46 @@
  * Time: 1:07 PM
  */
 
-namespace CampusCRM\CampusContactBundle\Migrations\Schema\v1_0;
+namespace CampusCRM\CampusContactBundle\Migrations\Schema\v1_9;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
 use Oro\Bundle\EntityBundle\EntityConfig\DatagridScope;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
+use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
 
-class ExtendContactEntity implements Migration
+class ExtendContactEntity implements Migration, ExtendExtensionAwareInterface
 {
+    protected $extendExtension;
+
+    public function setExtendExtension(ExtendExtension $extendExtension)
+    {
+        $this->extendExtension = $extendExtension;
+    }
     /**
      * {@inheritdoc}
      */
     public function up(Schema $schema, QueryBag $queries)
     {
+
+        $this->extendExtension->addManyToOneRelation(
+            $schema,
+            'orocrm_contact',
+            'country_of_birth',
+            'oro_dictionary_country',
+            'name',
+            ['extend' => ['without_default' => true, 'is_extend' => true, 'owner' => ExtendScope::OWNER_CUSTOM]]
+        );
+
         $table = $schema->getTable('orocrm_contact');
+
         $table->addColumn(
             'date_of_baptism',
             'date',
             [
                 'oro_options' => [
-                    'entity' => [
-                        'label' => 'Date of Baptism',
-                        'description' => '',
-                    ],
                     'extend' => ['owner' => ExtendScope::OWNER_CUSTOM],
                     'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_FALSE],
                     'merge' => ['display' => true],
@@ -43,10 +58,6 @@ class ExtendContactEntity implements Migration
             'integer',
             [
                 'oro_options' => [
-                    'entity' => [
-                        'label' => 'Student ID',
-                        'description' => '',
-                    ],
                     'extend' => ['owner' => ExtendScope::OWNER_CUSTOM],
                     'datagrid' => ['is_visible' => DatagridScope::IS_VISIBLE_FALSE],
                     'merge' => ['display' => true],
