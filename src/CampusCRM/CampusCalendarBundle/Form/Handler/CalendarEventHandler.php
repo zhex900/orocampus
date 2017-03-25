7c13010274eb1e96d7220e416ad49b7f84527f65
Jake He <zhex900@gmail.com>
2017-03-25 22:31:00 +1100

    Copy EventName field value to Title field value. Set the default Title field value to 'Default title'

---
+++ src/CampusCRM/CampusCalendarBundle/Form/Handler/CalendarEventHandler.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace CampusCRM\CampusCalendarBundle\Form\Handler;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
+use Oro\Bundle\CalendarBundle\Manager\CalendarEvent\NotificationManager;
+use Oro\Bundle\CalendarBundle\Entity\Calendar;
+use Oro\Bundle\EntityBundle\Tools\EntityRoutingHelper;
+use Oro\Bundle\UserBundle\Entity\User;
+use Oro\Bundle\CalendarBundle\Form\Handler\CalendarEventHandler as BaseHandler;
+
+class CalendarEventHandler extends BaseHandler
+{
+    /**
+     * Process form
+     *
+     * @param  CalendarEvent $entity
+     *
+     * @return bool True on successful processing, false otherwise
+     *
+     * @throws AccessDeniedException
+     * @throws \LogicException
+     */
+    public function process(CalendarEvent $entity)
+    {
+        $request = $this->getRequest();
+
+        $this->checkPermission($entity);
+
+        $this->form->setData($entity);
+
+        if (in_array($request->getMethod(), array('POST', 'PUT'))) {
+            // clone entity to have original values later
+            $originalEntity = clone $entity;
+
+            $this->ensureCalendarSet($entity);
+
+            $this->form->submit($request);
+
+            if ($this->form->isValid()) {
+                // TODO: should be refactored after finishing BAP-8722
+                // Contexts handling should be moved to common for activities form handler
+                if ($this->form->has('contexts')) {
+                    $contexts = $this->form->get('contexts')->getData();
+                    $owner = $entity->getCalendar() ? $entity->getCalendar()->getOwner() : null;
+                    if ($owner && $owner->getId()) {
+                        $contexts = array_merge($contexts, [$owner]);
+                    }
+                    $this->activityManager->setActivityTargets($entity, $contexts);
+                } elseif (!$entity->getId() && $entity->getRecurringEvent()) {
+                    $this->activityManager->setActivityTargets(
+                        $entity,
+                        $entity->getRecurringEvent()->getActivityTargetEntities()
+                    );
+                }
+
+                $this->processTargetEntity($entity, $request);
+
+                $title = $this->getForm()->get('oro_eventname')->getData();
+                $entity->setTitle($title);
+                //var_dump('title: '. $title);
+
+                $this->onSuccess($entity, $originalEntity);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
