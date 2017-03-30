f4efb446c732a6f93327760f2bfe668ea2009daf
Jake He <zhex900@gmail.com>
2017-03-30 23:05:49 +1100

    Remove services.yml. This is not used. z

--- src/CampusCRM/CampusCalendarBundle/DependencyInjection/CampusCalendarExtension.php
+++ src/CampusCRM/CampusCalendarBundle/DependencyInjection/CampusCalendarExtension.php
@@ -25,7 +25,7 @@ class CampusCalendarExtension extends Extension
         $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+       // $loader->load('services.yml');
         $loader->load('form.yml');
         $loader->load('form_types.yml');
     }
