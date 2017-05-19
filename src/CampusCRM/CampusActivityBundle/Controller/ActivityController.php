b2bc7b9094c40af19b5bb66355700a43af1e59bc
Jake He <zhex900@gmail.com>
2017-05-20 00:07:56 +1000

    Clean up code. Make a new function filter under Manger to filter User and Contact in Calendar Event.

--- src/CampusCRM/CampusActivityBundle/Controller/ActivityController.php
+++ src/CampusCRM/CampusActivityBundle/Controller/ActivityController.php
@@ -3,42 +3,15 @@
 namespace CampusCRM\CampusActivityBundle\Controller;
 
 use Doctrine\Common\Util\ClassUtils;
-use Oro\Bundle\ActivityBundle\Manager\ActivityManager;
-use Oro\Bundle\DataGridBundle\Provider\MultiGridProvider;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Oro\Bundle\ActivityBundle\Controller\ActivityController as BaseController;
-
 
 class ActivityController extends BaseController
 {
-
-    /**
-     * @param object $entity
-     *
-     * @return array
-     * [
-     *     [
-     *         'label' => label,
-     *         'gridName' => gridName,
-     *         'className' => className,
-     *     ],
-     * ]
-     */
     protected function getSupportedTargets($entity)
     {
         $entityClass = ClassUtils::getClass($entity);
         $targetClasses = array_keys($this->getActivityManager()->getActivityTargets($entityClass));
-
-        $userClass = 'Oro\Bundle\UserBundle\Entity\User';
-        $contactClass = 'Oro\Bundle\ContactBundle\Entity\Contact';
-
-        if ($entityClass == 'Oro\Bundle\CalendarBundle\Entity\CalendarEvent'){
-            $targetClasses = array_diff( $targetClasses, [$userClass,$contactClass] );
-        }
+        $targetClasses = $this->get('campus_activity.manger.filter')->filter($entityClass,$targetClasses);
 
         return $this->getMultiGridProvider()->getEntitiesData($targetClasses);
     }
