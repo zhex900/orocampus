ab24b4ad0a8dff83f07d4daa1ebd0b2e1aa4865e
Jake He <zhex900@gmail.com>
2017-05-22 17:07:53 +1000

    Fix attendance button bug.

--- src/CampusCRM/CampusCalendarBundle/Controller/CalendarEventController.php
+++ src/CampusCRM/CampusCalendarBundle/Controller/CalendarEventController.php
@@ -5,10 +5,7 @@ namespace CampusCRM\CampusCalendarBundle\Controller;
 use Oro\Bundle\CalendarBundle\Controller\CalendarEventController as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Oro\Bundle\SecurityBundle\Annotation\Acl;
-use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 /**
  * @Route("/event")
  */
@@ -23,9 +20,11 @@ class CalendarEventController extends BaseController
     protected function update(CalendarEvent $entity, $formAction)
     {
         $saved = false;
+        $select = false;
 
         if ($this->get('campus_calendar.calendar_event.form.handler')->process($entity)) {
             if (!$this->getRequest()->get('_widgetContainer')) {
+
                 $this->get('session')->getFlashBag()->add(
                     'success',
                     $this->get('translator')->trans('oro.calendar.controller.event.saved.message')
@@ -36,35 +35,16 @@ class CalendarEventController extends BaseController
             $saved = true;
         }
 
+        if( $this->getRequest()->get('select')){
+            $select = true;
+        }
+
         return [
             'entity'     => $entity,
             'saved'      => $saved,
             'form'       => $this->get('campus_calendar.calendar_event.form.handler')->getForm()->createView(),
-            'formAction' => $formAction
+            'formAction' => $formAction,
+            'attendance_button' => $select
         ];
-    }
-
-    /**
-     * @Route(
-     *          "/attendance/update/{id}",
-     *           name="oro_calendar_event_attendance",
-     *           requirements={"id"="\d+"},
-     *           condition="request.get('_widgetContainer')"
-     *       )
-     * @Template
-     * @Acl(
-     *      id="oro_calendar_event_attendance",
-     *      type="entity",
-     *      class="OroCalendarBundle:CalendarEvent",
-     *      permission="EDIT",
-     *      group_name=""
-     * )
-     * @AclAncestor("oro_calendar_event_update")
-     */
-    public function updateAction(CalendarEvent $entity)
-    {
-        return array_merge(
-                            parent::updateAction($entity),
-                            ['attendance_selection' => true]);
     }
 }
