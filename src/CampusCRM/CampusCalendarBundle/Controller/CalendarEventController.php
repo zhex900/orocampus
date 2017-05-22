e20ba732ff53df055281f4af7b0e08a6dc91bbc1
Jake He <zhex900@gmail.com>
2017-05-22 13:02:34 +1000

    Merge widget for Calendar events using in Activity and Select Attendance button.

--- src/CampusCRM/CampusCalendarBundle/Controller/CalendarEventController.php
+++ src/CampusCRM/CampusCalendarBundle/Controller/CalendarEventController.php
@@ -5,7 +5,10 @@ namespace CampusCRM\CampusCalendarBundle\Controller;
 use Oro\Bundle\CalendarBundle\Controller\CalendarEventController as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 /**
  * @Route("/event")
  */
@@ -39,5 +42,29 @@ class CalendarEventController extends BaseController
             'form'       => $this->get('campus_calendar.calendar_event.form.handler')->getForm()->createView(),
             'formAction' => $formAction
         ];
+    }
+
+    /**
+     * @Route(
+     *          "/attendance/update/{id}",
+     *           name="oro_calendar_event_attendance",
+     *           requirements={"id"="\d+"},
+     *           condition="request.get('_widgetContainer')"
+     *       )
+     * @Template
+     * @Acl(
+     *      id="oro_calendar_event_attendance",
+     *      type="entity",
+     *      class="OroCalendarBundle:CalendarEvent",
+     *      permission="EDIT",
+     *      group_name=""
+     * )
+     * @AclAncestor("oro_calendar_event_update")
+     */
+    public function updateAction(CalendarEvent $entity)
+    {
+        return array_merge(
+                            parent::updateAction($entity),
+                            ['attendance_selection' => true]);
     }
 }
