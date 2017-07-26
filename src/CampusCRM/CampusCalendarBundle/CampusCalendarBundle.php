f711e4ee6b41a98da3f345756d029c14f64e415c
Jake He <zhex900@gmail.com>
2017-07-26 22:33:54 +1000

    Copy parent event fields to child events.

--- src/CampusCRM/CampusCalendarBundle/CampusCalendarBundle.php
+++ src/CampusCRM/CampusCalendarBundle/CampusCalendarBundle.php
@@ -10,11 +10,22 @@
 namespace CampusCRM\CampusCalendarBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use CampusCRM\CampusCalendarBundle\DependencyInjection\Compiler\OverrideServiceCompilerPass;
 
 class CampusCalendarBundle extends Bundle
 {
     public function getParent()
     {
         return 'OroCalendarBundle';
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new OverrideServiceCompilerPass());
     }
 }
