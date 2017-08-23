9a5a4c7c51a2fc04f17d91c428b74d6307b83f2a
Jake He <zhex900@gmail.com>
2017-08-23 13:54:58 +1000

    signup page.

--- docker/images/oromono/bin/fix_parameters.sh
+++ docker/images/oromono/bin/fix_parameters.sh
@@ -10,3 +10,10 @@ echo '    redis_dsn_session:  'redis://redis:6379/1'' >> ${PARAMETERS}
 echo '    redis_dsn_doctrine: 'redis://redis:6379/2'' >> ${PARAMETERS}
 sed -i 's/websocket_frontend_port.*//g' ${PARAMETERS}
 echo '    websocket_frontend_port: 3088' >> ${PARAMETERS}
+
+  #  mailer_transport: smtp
+  #  mailer_host: smtp.zoho.com
+  #  mailer_port: 465
+  #  mailer_encryption: ssl
+  #  mailer_user: admin@unswchristians.com
+  #  mailer_password: fheman123
--- docker/images/oromono/bin/install-oro.sh
+++ docker/images/oromono/bin/install-oro.sh
@@ -21,7 +21,7 @@ rm -r /var/www/app/cache/*
 php /var/www/app/console cache:clear --env=prod -vvv
 
 chown -R www-data:www-data /var/www/ /srv/app-data/
-php /var/www/app/console oro:install --env=prod --user-name=admin --timeout=3000 --user-email=zhex900@gmail.com --user-firstname=Jake --user-lastname=He --user-password=Fheman123 --sample-data=n --organization-name=OROCAMPUS --application-url=http://orocampus.tk
+php /var/www/app/console oro:install --env=prod --user-name=admin --timeout=3000 --user-email=zhex900@gmail.com --user-firstname=Jake --user-lastname=He --user-password=Fheman123 --sample-data=n --organization-name=OROCAMPUS --application-url=http://app.orocampus.tk
 
 fix_parameters.sh
 
