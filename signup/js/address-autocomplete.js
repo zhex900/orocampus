2be9d0fa8fd8d590cdfc17539c6df075221bb410
Jake He <zhex900@gmail.com>
2017-08-24 21:56:51 +1000

    remove unused variable placeSearch

--- signup/js/address-autocomplete.js
+++ signup/js/address-autocomplete.js
@@ -7,7 +7,7 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-var placeSearch, autocomplete;
+var autocomplete;
 var componentForm = {
     street_number: 'short_name',
     route: 'long_name',
