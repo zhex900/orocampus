6d41cc03c9a67af7d26dc37c1a1c74246ded190c
Jake He <zhex900@gmail.com>
2017-08-25 23:04:04 +1000

    New reload button to download select field values from server.

--- signup/init.php
+++ signup/init.php
@@ -6,17 +6,46 @@
  * Time: 3:40 PM
  */
 
-// session started in zurmo.php
 include("orocampus.php");
-// starting a session to enable session variables to be stored
-session_start();
 
-$formType = $_REQUEST['form_types'];
-$_SESSION['form'] = "forms/" . $formType;
-$_SESSION['contactSource'] = $_REQUEST['source_of_contact'];
-$_SESSION['countries'] = json_decode(file_get_contents("./data/countries.json"), true);
+if (isset($_POST['reload'])){
+    // download selection values from server
+    $api = new ApiRest(URL,LOGIN,APIKEY);
 
-file_put_contents('/tmp/signup.log','source: '. $_SESSION['contactSource'].PHP_EOL,FILE_APPEND);
+    get($api,SOURCE);
+    get($api,COUNTRIES);
+    get($api,DEGREES);
+    get($api,INSTITUTIONS);
+    get($api,LEVELOFSTUDY);
+    // redirect to the selected form.
+    header("Location: login.php");
 
-// redirect to the selected form.
-header("Location: forms/" . $formType);
+}
+
+if (isset($_POST['login'])) {
+
+    $formType = $_REQUEST['form_types'];
+    $_SESSION['form'] = "forms/" . $formType;
+    $_SESSION['contactSource'] = $_REQUEST['source_of_contact'];
+    $_SESSION['countries'] = json_decode(file_get_contents("./data/countries.json"), true);
+
+    file_put_contents('/tmp/signup.log','source: '. $_SESSION['contactSource'].PHP_EOL,FILE_APPEND);
+
+    // redirect to the selected form.
+    header("Location: forms/" . $formType);
+}
+
+function get($api,$source){
+    $result = $api->curl_req($source);
+    file_put_contents( './data/'.$source.'.json',json_encode(transform($result)));
+}
+
+function transform($result){
+    $array=null;
+    if (!empty($result)) {
+        foreach ($result['data'] as $item) {
+            $array[$item['attributes']['name']] = $item['id'];
+        }
+    }
+    return $array;
+}
