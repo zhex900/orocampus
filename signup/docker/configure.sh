15bc62cf487d88eb51729d3adbef54292a0c6d7e
Jake He <zhex900@gmail.com>
2017-08-31 15:57:33 +1000

    Update docker

--- signup/docker/configure.sh
+++ signup/docker/configure.sh
@@ -6,36 +6,7 @@ cd orocampus
 git checkout signup
 cd signup
 composer update --no-dev
-mkdir /app/log
-chmod 777 /app/data /app/log
+mkdir /app/orocampus/signup/log
+chmod 777 /app/orocampus/signup/data /app/orocampus/signup/log
 
 apk update && apk add certbot
-
-rm -rf /tmp/orocampus
-
-echo '
-server {
-    listen 80 default_server;
-    server_name signup.unswchristians.com docker;
-
-    root "/app";
-    index login.php;
-    return 301 https://$server_name$request_uri;
-    include /opt/docker/etc/nginx/vhost.common.d/*.conf;
-}
-
-##############
-# SSL
-##############
-
-server {
-    listen 443 default_server;
-
-    server_name  signup.unswchristians.com docker;
-
-    root "/app";
-    index login.php;
-
-    include /opt/docker/etc/nginx/vhost.common.d/*.conf;
-    include /opt/docker/etc/nginx/vhost.ssl.conf;
-}' >  /opt/docker/etc/nginx/vhost.conf
