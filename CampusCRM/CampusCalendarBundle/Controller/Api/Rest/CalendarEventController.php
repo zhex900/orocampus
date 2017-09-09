9318a30de79a5543defaf472621eb3730df721fa
Jake He <zhex900@gmail.com>
2017-09-10 03:05:35 +1000

    Add contact to attendee in a calendar event via api

--- CampusCRM/CampusCalendarBundle/Controller/Api/Rest/CalendarEventController.php
+++ CampusCRM/CampusCalendarBundle/Controller/Api/Rest/CalendarEventController.php
@@ -4,8 +4,17 @@ namespace CampusCRM\CampusCalendarBundle\Controller\Api\Rest;
 
 use FOS\RestBundle\Controller\Annotations\NamePrefix;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
+use Oro\Bundle\CalendarBundle\Entity\CalendarEvent;
+use Oro\Bundle\CalendarBundle\Entity\Attendee;
+use Oro\Bundle\ContactBundle\Entity\Contact;
+use Oro\Bundle\SecurityBundle\Exception\ForbiddenException;
 use Oro\Bundle\SoapBundle\Form\Handler\ApiFormHandler;
 use Oro\Bundle\CalendarBundle\Controller\Api\Rest\CalendarEventController as BaseController;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\Util\Codes;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 
 /**
  * @RouteResource("calendarevent")
@@ -19,5 +28,89 @@ class CalendarEventController extends BaseController
     public function getFormHandler()
     {
         return $this->get('campus_calendar.calendar_event.form.handler.api');
+    }
+
+    /**
+     * Add an exisitng contact to a calendar event as an attendee
+     *
+     * @param int $eventId Calendar event id
+     * @param int $contactId Contact id
+     *
+     * @Put("/calendarevents/{eventId}/attendee/{contactId}",
+     *     requirements={"eventId"="\d+", "contactId"="\d+"})
+     *
+     * @ApiDoc(
+     *      description="Add an existing contact as an attendee to a Calendar Event",
+     *      resource=true
+     * )
+     * @AclAncestor("oro_calendar_event_update")
+     *
+     * @return Response
+     */
+    public function putAddAttendeeAction($eventId, $contactId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /** @var CalendarEvent $event */
+        $event = $em->getRepository('OroCalendarBundle:CalendarEvent')->find($eventId);
+        /** @var Contact $contact */
+        $contact = $em->getRepository('OroContactBundle:Contact')->find($contactId);
+        $attendee = $this->getAttendeeByContact($event, $contact);
+
+        /*
+         * Both event and contact have to exist and
+         * Contact is not already an attendee
+         */
+        try {
+            if (isset($attendee)) {
+                $reason = 'Contact is already attending this event';
+                throw new ForbiddenException($reason);
+            }
+            $attendee = $this->createAttendee($contact);
+            $event->addAttendee($attendee);
+
+            $em->persist($attendee);
+            $em->persist($event);
+            $em->flush();
+
+            $view = $this->view('attendee id: ' . $attendee->getId(), Codes::HTTP_OK);
+
+        } catch (ForbiddenException $forbiddenEx) {
+            $view = $this->view(['reason' => $forbiddenEx->getReason()], Codes::HTTP_FORBIDDEN);
+        }
+        return $this->buildResponse($view, self::ACTION_UPDATE, ['id' => $eventId, 'entity' => $event]);
+    }
+
+    /**
+     * Get attendee of Calendar Event by related User of Attendee.
+     *
+     * @param CalendarEvent $event
+     * @param Contact $contact
+     *
+     * @return Attendee|null
+     */
+    protected function getAttendeeByContact(CalendarEvent $event, Contact $contact)
+    {
+        $attendees = $event->getAttendees();
+        foreach ($attendees as $attendee) {
+            if ($attendee->getContact() == $contact) {
+                return $attendee;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param Contact $contact
+     *
+     * @return Attendee
+     */
+    protected function createAttendee(Contact $contact)
+    {
+        $attendee = new Attendee();
+        $attendee->setDisplayName($contact->getFirstName() . ' ' . $contact->getLastName());
+        $attendee->setContact($contact);
+        $attendee->setEmail($contact->getEmail());
+
+        return $attendee;
     }
 }
