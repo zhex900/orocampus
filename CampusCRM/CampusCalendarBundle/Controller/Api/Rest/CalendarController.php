15f6f0d7f2117b33f6bfcc2ca284bc40e0957ff5
Jake He <zhex900@gmail.com>
2017-09-13 13:28:21 +1000

    New Calendar API to get all the user calendars

---
+++ CampusCRM/CampusCalendarBundle/Controller/Api/Rest/CalendarController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CampusCRM\CampusCalendarBundle\Controller\Api\Rest;
+
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\NamePrefix;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Util\Codes;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Oro\Bundle\CalendarBundle\Entity\Calendar;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Symfony\Component\HttpFoundation\Response;
+use Oro\Bundle\CalendarBundle\Controller\Api\Rest\CalendarController as BaseController;
+
+/**
+ * @RouteResource("calendar")
+ * @NamePrefix("oro_api_")
+ */
+class CalendarController extends BaseController
+{
+    /**
+     * Get Calendar of All the Users
+     *
+     * @Get("/calendars/all")
+     *
+     * @ApiDoc(
+     *      description="Get calendar of all the users",
+     *      resource=true
+     * )
+     * @AclAncestor("oro_calendar_view")
+     *
+     * @return Response
+     */
+    public function getAllAction()
+    {
+        /** @var array $calendars */
+        $calendars = $this->getDoctrine()->getManager()->getRepository('OroCalendarBundle:Calendar')->findAll();
+        $result=null;
+        foreach ($calendars as $calendar){
+            /** @var Calendar $calendar **/
+            $result[]=[
+                'id'=>$calendar->getId(),
+                'owner_name'=>$calendar->getOwner()->getFirstName(). ' '.$calendar->getOwner()->getLastName(),
+                'owner_id'=>$calendar->getId()];
+        }
+        return new Response(json_encode($result), Codes::HTTP_OK);
+    }
+}
