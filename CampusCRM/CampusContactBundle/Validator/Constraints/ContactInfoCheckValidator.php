65feb252c54b1827b36a8573d54ce3058c8f045a
Jake He <zhex900@gmail.com>
2017-09-13 10:39:25 +1000

    Enforce compulsory fields at the field level.

--- CampusCRM/CampusContactBundle/Validator/Constraints/ContactInfoCheckValidator.php
+++ CampusCRM/CampusContactBundle/Validator/Constraints/ContactInfoCheckValidator.php
@@ -37,65 +37,21 @@ class ContactInfoCheckValidator extends ConstraintValidator
             ));
         }
 
-        if (!$this->compulsoryFields($value)){
+        if (!$this->validateEmailorPhone($value)) {
             return;
         }
-        if (!$this->validateEmailorPhone($value)){
+        if (!$this->validateUniqueEmail($value)) {
             return;
         }
-        if (!$this->validateUniqueEmail($value)){
+        if (!$this->validateUniquePhone($value)) {
             return;
         }
-        if (!$this->validateUniquePhone($value)){
-            return;
-        }
-    }
-
-    /*
-     * return false if compulsory fields are not completed. Otherwise true.
-     * @param Contact $contact
-     * @return bool
-     */
-    private function compulsoryFields(Contact $contact){
-        if ($contact->getFirstName()===null || strlen($contact->getFirstName())==0){
-            $this->context->addViolation(
-                'A contact must have a first name.'
-            );
-            return false;
-        }
-        if ($contact->getLastName()===null || strlen($contact->getLastName())==0){
-            $this->context->addViolation(
-                'A contact must have a last name.'
-            );
-            return false;
-        }
-        if ($contact->getGender()===null){
-            $this->context->addViolation(
-                'Gender cannot be blank.'
-            );
-            return false;
-        }
-        if ($contact->getContactSource()===null){
-            $this->context->addViolation(
-                'Contact source cannot be blank.'
-            );
-            return false;
-        }
-        if ($contact->getChurchKid()===null){
-            $this->context->addViolation(
-                'Church kid cannot be blank.'
-            );
-            return false;
-        }
-        return true;
     }
 
     /*
     * @param Contact $contact
     */
     private function validateEmailorPhone(Contact $contact){
-        file_put_contents('/tmp/v.log', 'check email or contact');
-
         if ($contact->getEmails()->count() > 0 ||
             $contact->getPhones()->count() > 0) {
             return true;
@@ -119,12 +75,12 @@ class ContactInfoCheckValidator extends ConstraintValidator
             $result = $this->em->getRepository('OroContactBundle:ContactEmail')
                 ->findOneBy(array('email' => (string)$email));
 
-            if ($result!==null &&
-                 (isset($id) && $id != $result->getOwner()->getId()) ||
-                (!isset($id))){
-                $message = 'This email '. $email .' is already used by ' . $result->getOwner()->getFirstName(). ' '. $result->getOwner()->getLastName() . ' ' . $result->getOwner()->getSemesterContacted();
-                $this->context->addViolation($message);
-                return false;
+            if ($result!==null) {
+                if ((isset($id) && $id != $result->getOwner()->getId()) || (!isset($id))) {
+                    $message = 'This email ' . $email . ' is already used by ' . $result->getOwner()->getFirstName() . ' ' . $result->getOwner()->getLastName() . ' ' . $result->getOwner()->getSemesterContacted();
+                    $this->context->addViolation($message);
+                    return false;
+                }
             }
         }
         return true;
@@ -143,12 +99,12 @@ class ContactInfoCheckValidator extends ConstraintValidator
             $result = $this->em->getRepository('OroContactBundle:ContactPhone')
                 ->findOneBy(array('phone' => (string)$phone));
 
-            if ($result!==null &&
-                (isset($id) && $id != $result->getOwner()->getId()) ||
-                (!isset($id))){
-                $message = 'This phone number '. $phone .' is already used by ' . $result->getOwner()->getFirstName(). ' '. $result->getOwner()->getLastName() . ' ' . $result->getOwner()->getSemesterContacted();
-                $this->context->addViolation($message);
-                return false;
+            if ($result!==null) {
+                if ((isset($id) && $id != $result->getOwner()->getId()) || (!isset($id))) {
+                    $message = 'This phone number ' . $phone . ' is already used by ' . $result->getOwner()->getFirstName() . ' ' . $result->getOwner()->getLastName() . ' ' . $result->getOwner()->getSemesterContacted();
+                    $this->context->addViolation($message);
+                    return false;
+                }
             }
         }
         return true;
